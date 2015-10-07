be1d16e696fe36ed164421c079bced2d01adc551
Pompelmo <gallo.kate@gmail.com>
2015-10-07 22:50:12 +0200

    try if it works without bottle with website list (not just one)

--- src/try_json.py
+++ src/try_json.py
@@ -13,14 +13,19 @@ def main():
     # class for rank, len, score computation
     cs = CreateJson(corpora, tfidf, index, tfidf_dict, tfidf_web,
                     mean_dict, ball_tree, d2v_model, des_dict, w2v_model, key_dict)
-    while True:
-        print "insert website or 'stop' to exit"
-        website = raw_input("--> ")
-        if website == "stop":
-            break
+    # while True:
+    #     print "insert website or 'stop' to exit"
+    #     website = raw_input("--> ")
+    #     if website == "stop":
+    #         break
+    #     a = datetime.now()
+    #     print cs.get_json(website, ScoreFunc(), n=10, only_web=False)
+    #     print "time taken", datetime.now() - a
+
+    for i in range(0, 10):
         a = datetime.now()
-        print cs.get_json(website, ScoreFunc(), n=10, only_web=False)
-        print "time taken", datetime.now() - a
+        print cs.get_json(['www.spaziodati.eu', 'spaziodati.eu'], ScoreFunc(), n=20, only_web=False)
+        print "time taken iteration ", i, " : ", datetime.now() - a
 
 if __name__ == '__main__':
     main()
