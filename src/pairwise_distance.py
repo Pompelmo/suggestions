7848486b0a2d88e9168c47a46cd90c008ffdf680
Pompelmo <gallo.kate@gmail.com>
2015-10-07 22:50:56 +0200

    pairwise distance between websites list mean vector and suggested websites vector

--- src/pairwise_distance.py
+++ src/pairwise_distance.py
@@ -3,56 +3,49 @@
 # using the different models
 # ------------------------------------------------------------
 
-import numpy
+import numpy as np
 from gensim import matutils
 from math import sqrt
 
 
-def tfidf_distance(corpora, tfidf, tfidf_web, web_1, web_2, loss_weight):
+def tfidf_distance(corpora, tfidf, tfidf_web, mean_vec, web_2, loss_weight):
     """compute the distance (as a function of cosine similarity) between two websites using tfidf model"""
     try:
-        indx_1 = tfidf_web.values().index(web_1)            # try to get the index of the website
         indx_2 = tfidf_web.values().index(web_2)
     except ValueError:
         return loss_weight
 
-    doc_num_1 = tfidf_web.keys()[indx_1]                    # now get its id (same index)
     doc_num_2 = tfidf_web.keys()[indx_2]
 
-    bow_1 = corpora[doc_num_1]                               # transform it in bow
     bow_2 = corpora[doc_num_2]
 
-    tf_rap_1 = matutils.unitvec(tfidf[bow_1])                                 # get its tfidf representation
-    tf_rap_2 = matutils.unitvec(tfidf[bow_2])
+    tf_rap_2 = matutils.unitvec(tfidf[bow_2])       # get its tfidf representation
 
-    cosine_sim = min(matutils.cossim(tf_rap_1, tf_rap_2), 1.0)
+    cosine_sim = min(matutils.cossim(mean_vec, tf_rap_2), 1.0)
 
     return sqrt(2.0 * (1.0 - cosine_sim)) / 2.0               # return the distance of the two vectors
 
 
-def w2v_distance(mean_dict, web_1, web_2, loss_weight):
+def w2v_distance(mean_dict, mean_vec, web_2, loss_weight):
     """compute the distance (as a function of cosine similarity) between two websites using w2v model"""
     try:
-        vector_1 = numpy.array(mean_dict[web_1])        # already unit vectors by construction
-        vector_2 = numpy.array(mean_dict[web_2])
+        vector_2 = np.array(mean_dict[web_2])        # already unit vectors by construction
     except KeyError:
         return loss_weight                                  # return max distance if not found
 
-    return float(numpy.linalg.norm(vector_1 - vector_2)) / 2.0      # return the distance of the two vectors
+    return float(np.linalg.norm(mean_vec - vector_2)) / 2.0      # return the distance of the two vectors
 
 
-def d2v_distance(d2v_model, web_1, web_2, loss_weight):
+def d2v_distance(d2v_model, mean_vec, web_2, loss_weight):
     """compute the distance(as a function of cosine similarity) between two websites using d2v model"""
 
-    vector_1 = numpy.array(d2v_model.docvecs[web_1])
-    vector_2 = numpy.array(d2v_model.docvecs[web_2])
+    vector_2 = np.array(d2v_model.docvecs[web_2])
 
-    if len(vector_1) == 100 and len(vector_2) == 100:
+    if len(vector_2) == 100:
 
-        vec_unit_1 = vector_1 / numpy.linalg.norm(vector_1)
-        vec_unit_2 = vector_2 / numpy.linalg.norm(vector_2)
+        vec_unit_2 = vector_2 / np.linalg.norm(vector_2)
 
-        dist = numpy.linalg.norm(vec_unit_1 - vec_unit_2)
+        dist = np.linalg.norm(mean_vec - vec_unit_2)
 
         return dist / 2.0
 
