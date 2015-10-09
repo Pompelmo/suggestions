8a8e697d93ec4e29df649bfce48d3dbbbf5a08f0
Pompelmo <gallo.kate@gmail.com>
2015-10-09 11:36:32 +0200

    more similar to bottle prova

--- src/try_json.py
+++ src/try_json.py
@@ -3,6 +3,7 @@ from gen_json import CreateJson
 from ScoreFunc import ScoreFunc
 from datetime import datetime
 import json
+from collections import OrderedDict
 
 # load the models
 corpora, tfidf, index, tfidf_dict, tfidf_web, \
@@ -17,8 +18,20 @@ def main(weblist):
     """run the main for a not ordered example :)"""
 
     a = datetime.now()
-    pretty = json.dumps(cs.get_json(weblist, ScoreFunc(), n=10, only_web=False),
-                        indent=4, separators=(':', ','))
+    dictionary = cs.get_json(weblist, ScoreFunc(), n=10, only_web=False)         # get dictionary from c_json
+
+    # order everything by the total score
+    if dictionary:
+        dictionary_sort = OrderedDict(sorted(dictionary[u'output'].items(),
+                                             key=lambda x: x[1][u'total_score'])[:10])
+        # read it as a json object
+        json_obj = {'input_website_metadata': dictionary[u'input_website_metadata'],
+                    'output': [{'website': website, 'data': data} for website, data in dictionary_sort.iteritems()]}
+    else:
+        json_obj = {'error': 'websites not present in the models'}
+
+    pretty = json.dumps(json_obj, indent=4, separators=(',', ':'))
+
     print pretty
     print "time taken iteration ", datetime.now() - a
 
